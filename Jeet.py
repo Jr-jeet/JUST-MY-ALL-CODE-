1b83645424281bf31ea2af4ceff5138d93716c80
Jr-jeet <jeet62032@gmail.com>
2026-09-10 16:12:10 +0530

    Update Jeet.py

--- Jeet.py
+++ Jeet.py
@@ -3,8 +3,10 @@
 #   NUM=int(input("ENTER_"))
 #   if NUM==GUESS:
 #        print("YOU DID IT")
+
 #   if NUM > GUESS:
 #       print("YOU CHOICE LARGER NUMBER")
+
 #   else:
 #       NUM < GUESS :
 #       print("YOU CHOICE SMALLER NUMBER")
